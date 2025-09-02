8588b4fe893b10a34e78a4237237270ca61bb0f6
Daniel Rezende <rezendedaniel2002@gmail.com>
2025-09-01 21:21:59 -0300

    Feat:Adiciona o ReceitasRepository inteface e extende com o JPA

---
+++ src/main/java/org/example/nutricomebh/Receitas/ReceitasRepository.java
@@ -0,0 +1,6 @@
+package org.example.nutricomebh.Receitas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceitasRepository extends JpaRepository<receitasModel, Long> {
+}
