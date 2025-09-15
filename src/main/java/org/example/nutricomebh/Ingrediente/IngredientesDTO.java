6be4a4bc3057ecbb968040d28e9d3d355c7e9151
Daniel Rezende <rezendedaniel2002@gmail.com>
2025-09-15 19:40:42 -0300

    Feat: implementa o IngredienteDTO e o IngredienteMapper

---
+++ src/main/java/org/example/nutricomebh/Ingrediente/IngredientesDTO.java
@@ -0,0 +1,22 @@
+package org.example.nutricomebh.Ingrediente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.nutricomebh.Receitas.ReceitasModel;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IngredientesDTO {
+
+        private Long id;
+        private String nome;
+        private List<ReceitasModel> receita;
+
+
+}
---
+++ src/main/java/org/example/nutricomebh/Ingrediente/IngredientesMapper.java
@@ -0,0 +1,25 @@
+package org.example.nutricomebh.Ingrediente;
+
+
+import jakarta.persistence.Column;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngredientesMapper {
+
+    public IngredienteModel mapIngrediente(IngredientesDTO ingredientesDTO){
+        IngredienteModel ingredienteModel = new IngredienteModel();
+        ingredienteModel.setId(ingredientesDTO.getId());
+        ingredienteModel.setNome(ingredientesDTO.getNome());
+        ingredienteModel.setReceita(ingredientesDTO.getReceita());
+        return ingredienteModel;
+    }
+
+    public IngredientesDTO mapIngredienteModel(IngredienteModel ingredienteModel){
+        IngredientesDTO ingredientesDTO = new IngredientesDTO();
+        ingredientesDTO.setId(ingredienteModel.getId());
+        ingredientesDTO.setNome(ingredienteModel.getNome());
+        ingredientesDTO.setReceita(ingredienteModel.getReceita());
+        return ingredientesDTO;
+    }
+}
