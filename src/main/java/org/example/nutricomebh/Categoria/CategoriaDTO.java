201d2c8e523f2bff4bbb837de69864f4ab90d6c5
Daniel Rezende <rezendedaniel2002@gmail.com>
2025-09-17 11:23:43 -0300

    Feat: Adiciona o categoriaDTO e o categoriaMapper

---
+++ src/main/java/org/example/nutricomebh/Categoria/CategoriaDTO.java
@@ -0,0 +1,20 @@
+package org.example.nutricomebh.Categoria;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.nutricomebh.Receitas.ReceitasModel;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoriaDTO {
+
+
+    private int id;
+    private String nome;
+    private List<ReceitasModel> receitas;
+}
---
+++ src/main/java/org/example/nutricomebh/Categoria/CategoriaMapper.java
@@ -0,0 +1,23 @@
+package org.example.nutricomebh.Categoria;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoriaMapper {
+    public CategoriaModel mapCategoria(CategoriaDTO categoriaDTO){
+        CategoriaModel categoriaModel = new CategoriaModel();
+        categoriaModel.setId(categoriaDTO.getId());
+        categoriaModel.setNome(categoriaDTO.getNome());
+        categoriaModel.setReceitas(categoriaDTO.getReceitas());
+        return categoriaModel;
+    }
+    public  CategoriaDTO mapCategoriaModel(CategoriaModel categoriaModel){
+        CategoriaDTO categoriaDTO = new CategoriaDTO();
+        categoriaDTO.setId(categoriaModel.getId());
+        categoriaDTO.setNome(categoriaModel.getNome());
+        categoriaDTO.setReceitas(categoriaModel.getReceitas());
+        return categoriaDTO;
+    }
+
+}
