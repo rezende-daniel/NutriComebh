0fad340d6ff7c2c088205ddca2eb4bdf3dfb5c7d
Daniel Rezende <rezendedaniel2002@gmail.com>
2025-09-17 11:29:37 -0300

    Feat: adiciona o CategoriaRepository e extende o JPA repository

---
+++ src/main/java/org/example/nutricomebh/Categoria/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package org.example.nutricomebh.Categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<CategoriaModel,Long> {
+}
