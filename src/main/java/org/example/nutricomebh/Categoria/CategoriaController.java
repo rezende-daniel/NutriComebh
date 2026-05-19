14ec4d39b0a7e8ce97164ff4d64b044009029087
Daniel Rezende <rezendedaniel2002@gmail.com>
2026-05-19 14:05:22 -0300

    Doc: Documenta as rotas da CategoriaController

--- src/main/java/org/example/nutricomebh/Categoria/CategoriaController.java
+++ src/main/java/org/example/nutricomebh/Categoria/CategoriaController.java
@@ -1,6 +1,10 @@
 package org.example.nutricomebh.Categoria;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,15 +25,24 @@ public class CategoriaController {
 
     //Adicionar categoria
     @PostMapping("/adicionarCategoria")
-    public ResponseEntity<String> adicionarCategoria(@RequestBody CategoriaDTO  categoria){
+    @Operation(summary = "Cria uma categoria",description = "Cria uma nova categoria,estou utilizando o console do H2 para fazer a novas categorias, o sistema foi pensado para ter apenas duas ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Categoria criada com sucesso"),
+            @ApiResponse(responseCode = "400",description = "Erro ao crira a categoria")
+    })
+    public ResponseEntity<String> adicionarCategoria(@Parameter(description = "Usuario envia a nova categoria pelo corpo da requiseção") @RequestBody CategoriaDTO  categoria){
         CategoriaDTO novaCategoria = categoriaService.createCategoria(categoria);
         return  ResponseEntity.status(HttpStatus.CREATED).body(novaCategoria.toString());
     }
 
     //Deletar categoria
-
     @DeleteMapping("/deletarCategoria/{id}")
-    public ResponseEntity<String> deletarCategoria(@PathVariable Long id){
+    @Operation(summary = "Deleta categoria",description = "Deleta a categoria usando o ID como parametro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Categoria deletada com sucesso"),
+            @ApiResponse(responseCode = "400",description = "Erro ao deletar a categoria")
+    })
+    public ResponseEntity<String> deletarCategoria(@Parameter(description = "O ususario envia no corpo da requisição o Id para deletar a categoria")@PathVariable Long id){
         if(categoriaService.listarCategoriaId(id)!=null){
             categoriaService.deletarCategoria(id);
             return ResponseEntity.ok("Categoria deletada");
@@ -39,7 +52,12 @@ public class CategoriaController {
     }
     //Pesquisar categoria por id
     @GetMapping("/procurarCategoriaPorId/{id}")
-    public ResponseEntity<?> procurarCategoriaPorId(@PathVariable Long id){
+    @Operation(summary = "Pesquia a categoria",description = "Pesquisa a categoria usando o Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Sucesso ao pesquisa a categoria"),
+            @ApiResponse(responseCode = "400",description = "Erro ao pesquisar")
+    })
+    public ResponseEntity<?> procurarCategoriaPorId(@Parameter(description = "Usuario envia o ID para a pesquisa no corpo da requisição")@PathVariable Long id){
         if(categoriaService.listarCategoriaId(id)!=null){
             categoriaService.listarCategoriaId(id);
             return ResponseEntity.status(HttpStatus.OK).body("Categoria encontrada");
@@ -49,6 +67,11 @@ public class CategoriaController {
     }
     //Listar categorias
     @GetMapping("/mostrarCategoria")
+    @Operation(summary = "Lista categoria",description = "Lista as categorias para atrelar a receita")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Lista as categorias"),
+            @ApiResponse(responseCode = "400",description = "Erro ao listar as categorias")
+    })
     private ResponseEntity<List<CategoriaDTO>> mostrarCategoria(){
         List<CategoriaDTO> categorias =categoriaService.lisarCategorias();
         return ResponseEntity.status(HttpStatus.OK).body(categorias);
