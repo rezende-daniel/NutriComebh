3ce003169f79cb52007997ba5cc85212b46c4793
Daniel Rezende <rezendedaniel2002@gmail.com>
2026-05-21 20:02:17 -0300

    Doc:Documenta as rotas do QuantidadeController

--- src/main/java/org/example/nutricomebh/Quantidade/QuantidadeController.java
+++ src/main/java/org/example/nutricomebh/Quantidade/QuantidadeController.java
@@ -1,6 +1,10 @@
 package org.example.nutricomebh.Quantidade;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +22,20 @@ public class QuantidadeController {
 
     //Cria quantidade
     @PostMapping("/criaQuantidade")
-    public ResponseEntity<String> criaQuantidade(@RequestBody QuantidadeDTO quantidadeDTO) {
+    @Operation(summary = "Cria a quantidade",description = "Cria a quantidade para ser relacionada com um item dentro de uma receita")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Quantidade criada com sucesso"),
+            @ApiResponse(responseCode = "402",description = "Erro na criacao da quantidade")
+    })
+    public ResponseEntity<String> criaQuantidade(@Parameter(description = "O usario enviara a quantidade no corpo da requisicao para ser relacionada com o ingrediente")
+                                                     @RequestBody QuantidadeDTO quantidadeDTO) {
         QuantidadeDTO novaQuantidade = quantidadeService.criarQuantidade(quantidadeDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaQuantidade.toString());
     }
     //Alterar quantidade
     @PutMapping("/alterarQuantidade")
-    public ResponseEntity<String> alterarQuantidadade(@RequestBody QuantidadeDTO quantidadeDTO) {
+    @Operation(summary = "Altera a quantidade",description = "Altera a quantidade para o ingrediente")
+    public ResponseEntity<String> alterarQuantidadade(@Parameter(description = "O usuario envia a nova quantidade para ser atrelada ao item da receita") @RequestBody QuantidadeDTO quantidadeDTO) {
         quantidadeService.atualizarQuantidade(quantidadeDTO, quantidadeDTO.getId());
         return ResponseEntity.status(HttpStatus.OK).body("Quantidade alterado");
     }
