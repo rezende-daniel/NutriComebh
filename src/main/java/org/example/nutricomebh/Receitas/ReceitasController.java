8c4ce3e0c349a2f5c0848f3c13b873b7ecde8386
Daniel Rezende <rezendedaniel2002@gmail.com>
2026-05-25 11:11:53 -0300

    Doc: Documenta as rotas da ReceitaController

--- src/main/java/org/example/nutricomebh/Receitas/ReceitasController.java
+++ src/main/java/org/example/nutricomebh/Receitas/ReceitasController.java
@@ -1,6 +1,10 @@
 package org.example.nutricomebh.Receitas;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.example.nutricomebh.Ingrediente.IngredientesDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +30,12 @@ public class ReceitasController {
 
     //Adiciona receita
     @PostMapping("/criaReceita")
-    public ResponseEntity<String> criaReceita(@RequestBody ReceitasDTO receitas,@RequestBody List< IngredientesDTO> ingredientes){
+    @Operation(summary = "Cria uma receita",description = "Cria a receita com todas as entidades,item e categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Receita criada com sucesso"),
+            @ApiResponse(responseCode = "402",description = "Erro ao crira a receita")
+    })
+    public ResponseEntity<String> criaReceita(@Parameter(description = "O usuario enviara no corpo da requisicao toda a receita e suas entidades") @RequestBody ReceitasDTO receitas){
 
         ReceitasDTO novaReceita = receitasService.criarReceita(receitas);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaReceita.toString());
@@ -34,6 +43,11 @@ public class ReceitasController {
 
     //Deleta receita
     @DeleteMapping("/deletaReceita/{id}")
+    @Operation(summary = "Deleta a receita",description = "Deleta a receita do banco de dados usando o ID,esta rota nao esta presente na aplicacao por motivos de seguranca")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Receita removida com sucesso"),
+            @ApiResponse(responseCode = "404",description = "Erro ao deletar a receita")
+    })
     public ResponseEntity<String> deletaReceita(@PathVariable Long id){
         if (receitasService.listarReceitasPorId(id) == null){
             receitasService.deletarReceita(id);
@@ -44,7 +58,12 @@ public class ReceitasController {
     }
     //Procura receita por ID
     @GetMapping("/procurarReceitaPorId/{id}")
-    private ResponseEntity<?> procurarReceitaPorId(@PathVariable Long id){
+    @Operation(summary = "Procura receita por Id",description = "Usa o id passado pelo usuario para fazer a pesquisa no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Pesquisa por Id realizada com sucesso"),
+            @ApiResponse(responseCode = "402",description = "Erro ao procurar receita por id")
+    })
+    private ResponseEntity<?> procurarReceitaPorId(@Parameter(description = "Usuario passa o id no corpo da requisicao") @PathVariable Long id){
         if (receitasService.listarReceitasPorId(id) != null){
             receitasService.listarReceitasPorId(id);
 
@@ -55,7 +74,13 @@ public class ReceitasController {
     }
     //Edita receita
     @PutMapping("/editarReceita/{id}")
-    public ResponseEntity<String> editaReceita(@RequestBody ReceitasDTO receitaAtualizada,@PathVariable Long id){
+    @Operation(summary = "Edita receita",description = "Edita a receita escolhida pelo usuario, atualmente ela sobrescreve a receita antiga")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Receita editada com sucesso"),
+            @ApiResponse(responseCode = "404",description = "Erro ao editar a receita")
+    })
+    public ResponseEntity<String> editaReceita(@Parameter(description = "Usuario manda no corpo da requisicao a receita editada") @RequestBody ReceitasDTO receitaAtualizada,
+                                               @Parameter(description = "Id vem no corpo da requisicao e sera usado para identificar a receita a ser sobre escrita") @PathVariable Long id){
         if (receitasService.listarReceitasPorId(id) != null){
             receitasService.editarReceita(receitaAtualizada, id);
             return ResponseEntity.status(HttpStatus.OK).body("Receita editada");
@@ -65,13 +90,23 @@ public class ReceitasController {
     }
     //Listar receitas
     @GetMapping("/mostrarAsReceitas")
+    @Operation(summary = "Lista todas as receitas",description = "Lista todas as receitas cadastradas no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Listagem feita com sucesso"),
+            @ApiResponse(responseCode = "404",description = "Erro ao listar as receitas")
+    })
     private ResponseEntity<List<ReceitasDTO>> mostrarAsReceitas(){
         List<ReceitasDTO> receitas = receitasService.listaReceitas();
         return ResponseEntity.status(HttpStatus.OK).body(receitas);
     }
     //Listar receita por categoria
     @GetMapping("/mostrarPorCategoria")
-    public ResponseEntity<List<ReceitasDTO>> mostrarPorCategoria( Long categoria){
+    @Operation(summary = "Lista as receitas por Categoria",description = "Pesquisa e lista as receitas pela categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Listagem por categoria feita com sucesso"),
+            @ApiResponse(responseCode = "404",description = "Listagem por categoria falhou")
+    })
+public ResponseEntity<List<ReceitasDTO>> mostrarPorCategoria(@Parameter(description = "Sistema utiliza a categoria para montar a lista de receitas no cardapio") @RequestBody Long categoria){
         List <ReceitasDTO> receitasCategoria = receitasService.listarReceitaPorCategoria(categoria);
 
         return ResponseEntity.status(HttpStatus.OK).body(receitasCategoria);
